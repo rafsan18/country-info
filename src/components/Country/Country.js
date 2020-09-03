38a0571e723ae2f48c63f4adc21f508e27aef227
rafsan <rafsanamin18@gmail.com>
2020-09-04 00:11:42 +0600

    final commit

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,19 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 const Country = (props) => {
   //console.log(props);
   const { name, capital, flag } = props.country;
+  const history = useHistory();
+
+  const handleButton = (countryName) => {
+    const url = `/country/${countryName}`;
+    history.push(url);
+  };
 
   return (
     <div className="col-md-3 col-sm-6">
       <div className="border p-3 mb-3">
-        <img className="img-fluid rounded" src={flag} alt="" />
+        <img className="img-fluid rounded border" src={flag} alt="" />
         <h1>{name}</h1>
         <p>Capital: {capital}</p>
         <p>
           <Link to={`/country/${name}`}>See Details</Link>
         </p>
+        <button onClick={() => handleButton(name)} className="btn btn-primary">
+          See Details
+        </button>
       </div>
     </div>
   );
--- src/components/CountryDetails/CountryDetails.js
+++ src/components/CountryDetails/CountryDetails.js
@@ -5,17 +5,24 @@ import { useEffect } from "react";
 const CountryDetails = () => {
   const { countryName } = useParams();
   const [country, setCountry] = useState([]);
+  const { name, capital, flag, area, population, region } = country;
 
   useEffect(() => {
     const url = `https://restcountries.eu/rest/v2/name/${countryName}`;
+
     fetch(url)
       .then((res) => res.json())
-      .then((data) => setCountry(data));
+      .then((data) => setCountry(data[0]));
   }, []);
-  console.log(country);
+
   return (
-    <div>
-      <h3>This is country</h3>
+    <div className="container mt-3">
+      <h2 className="text-center">{name}</h2>
+      <img className="img-fluid border rounded" src={flag} alt="" />
+      <h4 className="mt-5">Capital: {capital}</h4>
+      <p>Area: {area}</p>
+      <p>Population: {population}</p>
+      <p>Region: {region}</p>
     </div>
   );
 };
